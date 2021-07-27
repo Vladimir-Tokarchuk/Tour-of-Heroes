0aa601de4611e32a6b76073a01809b519954525e
Vladimir Tokarchuk <v.tokarchuk@do-it.co>
2021-07-28 01:15:00 +0300

    handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
-
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
@@ -14,18 +14,36 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService) { }
     private heroesUrl = 'api/heroes';
+    private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    
+    console.error(error); // log to console instead
+
+    
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 
     getHeroes(): Observable<Hero[]> {
       return this.http.get<Hero[]>(this.heroesUrl)
+        .pipe(
+          tap(_ => this.log('fetched heroes')),
+          catchError(this.handleError<Hero[]>('getHeroes', []))
+        );
     }
 
-  getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
-  }
+  /** GET hero by id. Will 404 if id not found */
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
+}
   
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
