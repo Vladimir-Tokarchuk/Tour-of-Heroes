b71d018adf479ae987568fe7fbae38183abe88e7
Vladimir Tokarchuk <v.tokarchuk@do-it.co>
2021-07-28 16:35:25 +0300

    update, add and delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -36,7 +36,7 @@ export class HeroService {
         );
     }
 
-  /** GET hero by id. Will 404 if id not found */
+/** GET hero by id. Will 404 if id not found */
 getHero(id: number): Observable<Hero> {
   const url = `${this.heroesUrl}/${id}`;
   return this.http.get<Hero>(url).pipe(
@@ -48,4 +48,32 @@ getHero(id: number): Observable<Hero> {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+updateHero(hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
+
+/** DELETE: delete the hero from the server */
+deleteHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+
+  return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    tap(_ => this.log(`deleted hero id=${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
+  );
+}
+httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 }
